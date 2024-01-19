3cc472363b088ea922872a8c2e7f0b080f0bb71a
Aurelien229 <blampainaurelien229@gmail.com>
2024-01-19 11:24:32 +0100

    update photo

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
+// Importer la fonction createTemperatureChart depuis le fichier graphic.js
+import { createTemperatureChart } from '../js/graphic.js';
+// Importer la fonction getCityPhoto depuis le fichier unsplash.js
+import { getCityPhoto } from '../js/unsplash.js'; // Assurez-vous de spécifier le chemin correct
+
+// Clé API OpenWeatherMap
+const apiKey = "e712e2a639cd686c277fc5416e3d6590";
+
+// Clé API Unsplash
+const unsplashApiKey = "CH-JOMBQJgVhJiJ_ae0nY5r-eKERC_ySua7FidoxQgg";
+
 // Déclarer les tableaux à un niveau supérieur
 let temperatureData = [];
 let humidityData = [];
 let windData = [];
-
-// Importer la fonction createTemperatureChart depuis le fichier graphic.js
-import { createTemperatureChart } from '../js/graphic.js';
-
-// Clé API OpenWeatherMap
-const apiKey = "e712e2a639cd686c277fc5416e3d6590";
+let sommeTemperature = 0;
+let joursAafficher;
 
 // Récupérer l'élément d'entrée du texte dans le DOM
 const textInput = document.getElementById('inputCity');
@@ -20,97 +27,103 @@ if (savedCity) {
     textInput.value = savedCity;
 }
 
-// Fonction pour effectuer l'appel à l'API OpenWeatherMap
-function apiCall(city) {
-    // Construire l'URL pour l'appel API avec la ville spécifiée
-    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}&lang=fr`;
+// Fonction pour effectuer l'appel à l'API OpenWeatherMap et Unsplash
+async function apiCall(city) {
+    try {
+        // Construire l'URL pour l'appel API OpenWeatherMap avec la ville spécifiée
+        let openWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}&lang=fr`;
 
-    // Récupérer le conteneur une seule fois en dehors de la boucle
-    let conteneurInfo = document.querySelector(".info_container");
+        // Récupérer le conteneur une seule fois en dehors de la boucle
+        let conteneurInfo = document.querySelector(".info_container");
 
-    // Vider le contenu existant du conteneur
-    conteneurInfo.innerHTML = "";
+        // Vider le contenu existant du conteneur
+        conteneurInfo.innerHTML = "";
 
-    // Effectuer la requête fetch vers l'API OpenWeatherMap
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            // Créer un élément h2 pour afficher le nom de la ville
-            let nomVille = document.createElement("h2");
-            nomVille.innerHTML = `${city}`;
+        // Effectuer la requête fetch vers l'API OpenWeatherMap
+        const weatherResponse = await fetch(openWeatherUrl);
+        const weatherData = await weatherResponse.json();
 
-            // Vérifier si l'élément ville existe, sinon créer un nouveau conteneur
-            let ville = document.getElementById("ville");
-            if (!ville) {
-                ville = document.createElement("div");
-                ville.id = "ville";
-                document.body.appendChild(ville);
-            }
+        // Créer un élément h2 pour afficher le nom de la ville
+        let nomVille = document.createElement("h2");
+        nomVille.innerHTML = `${city}`;
 
-            // Ajouter le nom de la ville à l'élément ville
-            ville.innerHTML = "";
-            ville.appendChild(nomVille);
+        // Vérifier si l'élément ville existe, sinon créer un nouveau conteneur
+        let ville = document.getElementById("ville");
+        if (!ville) {
+            ville = document.createElement("div");
+            ville.id = "ville";
+            document.body.appendChild(ville);
+        }
 
-            // Définir le nombre de jours à afficher
-            let joursAafficher = 5;
+        // Ajouter le nom de la ville à l'élément ville
+        ville.innerHTML = "";
+        ville.appendChild(nomVille);
 
-            // Boucle pour afficher les données pour chaque jour
-            let sommeTemperature = 0;
+        // Appeler la fonction pour récupérer une image depuis Unsplash
+        const cityPhotoUrl = await getCityPhoto(city);
 
-            for (let j = 0; j < joursAafficher; j++) {
-                // Calculer l'indice correspondant à la journée actuelle
-                const i = j * 8;
+        // Vérifier si une URL d'image est disponible
+        if (cityPhotoUrl) {
+            // Appliquer l'image en tant que fond pour l'élément h2
+            ville.style.backgroundImage = `url(${cityPhotoUrl})`;
+        }
 
-                // Créer un élément div pour afficher les informations de la journée
-                let jour = document.createElement("div");
-                jour.className = "infoWeek";
+        // Définir le nombre de jours à afficher
+        joursAafficher = 5;
 
-                // Remplir l'élément div avec les informations météorologiques
-                jour.innerHTML =
-                    `   
-                        <div id="date${j}" class="box"></div>
-                        <div id="temp${j}" class="box"></div>
-                        <div id="humidity${j}" class="box"></div>
-                        <div id="wind${j}" class="box"></div>
-                    `;
+        // Boucle pour afficher les données pour chaque jour
+        for (let j = 0; j < joursAafficher; j++) {
+            // Calculer l'indice correspondant à la journée actuelle
+            const i = j * 8;
 
-                // Ajouter le jour au conteneur
-                conteneurInfo.appendChild(jour);
+            // Créer un élément div pour afficher les informations de la journée
+            let jour = document.createElement("div");
+            jour.className = "infoWeek";
 
-                // Remplir chaque boîte avec les données météorologiques correspondantes
-                let date = new Date(data.list[i].dt * 1000);
-                document.querySelector(`#date${j}`).innerHTML = date.toLocaleDateString();
-                document.querySelector(`#temp${j}`).innerHTML = "<i class='fa-solid fa-temperature-empty'></i>" + data.list[i].main.temp + "°";
-                document.querySelector(`#humidity${j}`).innerHTML = "<i class='fa-solid fa-droplet'></i>" + data.list[i].main.humidity + "%";
-                document.querySelector(`#wind${j}`).innerHTML = "<i class='fa-solid fa-wind'></i>" + data.list[i].wind.speed + "km/h";
+            // Remplir l'élément div avec les informations météorologiques
+            jour.innerHTML =
+                `   
+                    <div id="date${j}" class="box"></div>
+                    <div id="temp${j}" class="box"></div>
+                    <div id="humidity${j}" class="box"></div>
+                    <div id="wind${j}" class="box"></div>
+                `;
 
-                // Ajouter les données à chaque tableau
-                temperatureData.push(data.list[i].main.temp);
-                humidityData.push(data.list[i].main.humidity);
-                windData.push(data.list[i].wind.speed);
+            // Ajouter le jour au conteneur
+            conteneurInfo.appendChild(jour);
 
-                // Ajouter la température à la somme
-                sommeTemperature += data.list[i].main.temp;
-            }
+            // Remplir chaque boîte avec les données météorologiques correspondantes
+            let date = new Date(weatherData.list[i].dt * 1000);
+            document.querySelector(`#date${j}`).innerHTML = date.toLocaleDateString();
+            document.querySelector(`#temp${j}`).innerHTML = "<i class='fa-solid fa-temperature-empty'></i>" + weatherData.list[i].main.temp + "°";
+            document.querySelector(`#humidity${j}`).innerHTML = "<i class='fa-solid fa-droplet'></i>" + weatherData.list[i].main.humidity + "%";
+            document.querySelector(`#wind${j}`).innerHTML = "<i class='fa-solid fa-wind'></i>" + weatherData.list[i].wind.speed + "km/h";
 
-            // Appeler la fonction pour créer le graphique
-            createTemperatureChart(temperatureData, humidityData, windData);
+            // Ajouter les données à chaque tableau
+            temperatureData.push(weatherData.list[i].main.temp);
+            humidityData.push(weatherData.list[i].main.humidity);
+            windData.push(weatherData.list[i].wind.speed);
 
-            // Calculer la moyenne des températures
-            const moyenneTemperature = sommeTemperature / joursAafficher;
+            // Ajouter la température à la somme
+            sommeTemperature += weatherData.list[i].main.temp;
+        }
 
-            // Changer le fond en fonction de la moyenne des températures
-            if (moyenneTemperature >= 0) {
-                document.body.style.backgroundImage = "url('assets/three.jpg')"; // Remplacez 'background1.jpg' par le chemin de votre image
-            } else {
-                document.body.style.backgroundImage = "url('assets/snow2.jpg')"; // Remplacez 'background2.jpg' par le chemin de votre image
-            }
+        // Appeler la fonction pour créer le graphique
+        createTemperatureChart(temperatureData, humidityData, windData);
 
-        })
-        .catch(err => {
-            // Gérer les erreurs lors de l'appel API
-            console.error("Erreur : " + err);
-        });
+        // Calculer la moyenne des températures
+        const moyenneTemperature = sommeTemperature / joursAafficher;
+
+        // Changer le fond en fonction de la moyenne des températures
+        if (moyenneTemperature >= 0) {
+            document.body.style.backgroundImage = `url('assets/three.jpg')`;
+        } else {
+            document.body.style.backgroundImage = `url('assets/snow2.jpg')`;
+        }
+
+    } catch (error) {
+        console.error("Erreur lors de l'appel à l'API : " + error);
+    }
 }
 
 // Attacher un gestionnaire d'événements lorsque le DOM est chargé
@@ -133,3 +146,4 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // Appeler initialement la fonction API avec une ville par défaut (Erquelinnes)
 apiCall("Erquelinnes");
+
---
+++ js/unsplash.js
@@ -0,0 +1,29 @@
+// Clé API Unsplash
+const unsplashApiKey = "CH-JOMBQJgVhJiJ_ae0nY5r-eKERC_ySua7FidoxQgg";
+
+// Fonction pour récupérer une photo de la ville depuis l'API Unsplash
+export async function getCityPhoto(city) {
+    const unsplashUrl = `https://api.unsplash.com/photos/random?query=${city}&client_id=${unsplashApiKey}`;
+
+    try {
+        const response = await fetch(unsplashUrl);
+        const data = await response.json();
+
+        // Vérifier si l'objet data contient la propriété errors
+        if (data.errors && data.errors.length > 0) {
+            console.error("Erreur lors de la récupération de la photo de la ville :", data.errors[0]);
+            return null;
+        }
+
+        // Récupérer l'URL de l'image directement depuis l'objet de réponse
+        if (data.urls && data.urls.full) {
+            return data.urls.full; // Vous pouvez utiliser 'full' ou d'autres propriétés selon vos besoins
+        } else {
+            console.error("La propriété 'urls.full' est indéfinie dans la réponse de l'API Unsplash.");
+            return null;
+        }
+    } catch (error) {
+        console.error("Erreur lors de la récupération de la photo de la ville : " + error);
+        return null;
+    }
+}
